016e49eff14d03539af23698943dd541ec1025f7
Smit Patel <46293548+smit25@users.noreply.github.com>
2020-01-11 00:34:12 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,14 +5,14 @@ import { Redirect} from 'react-router-dom'
 import firebase from 'firebase';
 import './form.css'
   const firebaseConfig = {
-    apiKey: "AIzaSyAePUsL9TYQ_Wd6AvbesU4XSuBT6uBueP0",
-    authDomain: "choco-25.firebaseapp.com",
-    databaseURL: "https://choco-25.firebaseio.com",
-    projectId: "choco-25",
-    storageBucket: "choco-25.appspot.com",
-    messagingSenderId: "919687701950",
-    appId: "1:919687701950:web:754ea7424dc336f353c5ef",
-    measurementId: "G-SED2VRFJ4N"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 firebase.initializeApp(firebaseConfig);
